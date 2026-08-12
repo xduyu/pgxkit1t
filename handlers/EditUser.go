f074d495fbc9118829c45ce67843e6d1430bee13
rodya <147410114+xduyu@users.noreply.github.com>
2026-08-12 23:40:04 +0500

    Update EditUser.go

--- handlers/EditUser.go
+++ handlers/EditUser.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	"database/sql"
-	"log"
 	"v1/test/fiber/db/helpers/db/helpers"
 	structures "v1/test/fiber/db/helpers/handlers/Structures"
 
@@ -13,9 +12,6 @@ func EditUser(database *sql.DB) fiber.Handler {
 	return func(c fiber.Ctx) error {
 		var req structures.HReqEditUser
 		c.Bind().Body(&req)
-		log.Print(req.Editable_UserId)
-		log.Print(req.Data)
-		log.Print(req.Data.Username)
 		resp := helpers.HelperEditUser(database, req.Editable_UserId, req.Data)
 		return c.Status(fiber.StatusOK).JSON(fiber.Map{
 			"info": resp,
